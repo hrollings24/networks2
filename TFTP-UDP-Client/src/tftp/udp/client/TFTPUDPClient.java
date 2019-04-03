e226eb5de6dbeb0d12a6b74d7dbca294cd57a0ec
Harry Rollings <harryrollings@icloud.com>
2019-04-03 15:03:51 +0100

    added UDP

--- TFTP-UDP-Client/src/tftp/udp/client/TFTPUDPClient.java
+++ TFTP-UDP-Client/src/tftp/udp/client/TFTPUDPClient.java
@@ -5,6 +5,12 @@
  */
 package tftp.udp.client;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 /**
  *
  * @author harryrollings
@@ -13,9 +19,52 @@ public class TFTPUDPClient {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        // parse arguments
+        // command
+        // command = read | write 
+        
+        String command = args[0];
+        switch (command){
+            case "write":
+                write();
+            case "read":
+                read();
+        }
+        
+    }
+
+    private static void read() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public static void write() throws IOException{
+        DatagramSocket socket;
+        DatagramPacket packet;
+        
+        int len = 512;
+        byte[] buf = new byte[len];
+        
+        socket = new DatagramSocket(4000);
+        
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        
+        packet = new DatagramPacket(buf, len);
+        packet.setAddress(address);
+        packet.setPort(9000);
+
+        socket.send(packet);
+
+        //ACK from server
+        socket.receive(packet);
+
+        // display response
+        String received = new String(packet.getData());
+        System.out.println("Today's date: " + received.substring(0, packet.getLength()));
+        socket.close();
+        
     }
     
 }
