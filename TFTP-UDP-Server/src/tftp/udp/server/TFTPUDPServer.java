62f465c49470a4411c4051bc0fdf51e1e1965375
Harry Rollings <harryrollings@icloud.com>
2019-04-30 18:02:33 +0100

    wrote a file!

--- TFTP-UDP-Server/src/tftp/udp/server/TFTPUDPServer.java
+++ TFTP-UDP-Server/src/tftp/udp/server/TFTPUDPServer.java
@@ -5,12 +5,17 @@
  */
 package tftp.udp.server;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -45,7 +50,8 @@ public class TFTPUDPServer extends Thread {
         
         System.out.println("IN SERVER");
         
-        int counter = 0;                    // just a counter - used below
+        
+        int counter=0;
         byte[] recvBuf = new byte[512];     // a byte array that will store the data received by the client
 
         try {
@@ -54,29 +60,105 @@ public class TFTPUDPServer extends Thread {
 
                 DatagramPacket packet = new DatagramPacket(recvBuf, 512);
                 socket.receive(packet);
-
-                // Get the current date/time and copy it in the byte array
-                String dString = new Date().toString() + " - Counter: " + (counter);
-                int len = dString.length();                                             // length of the byte array
-                byte[] buf = new byte[len];                                             // byte array that will store the data to be sent back to the client
-                System.arraycopy(dString.getBytes(), 0, buf, 0, len);
-
-                InetAddress addr = packet.getAddress();
-                int srcPort = packet.getPort();
-
-                packet.setData(buf);
                 
-                packet.setAddress(addr);
-                packet.setPort(srcPort);
+                String fileName = null;
+                char mode = (char) recvBuf[0];
+                System.out.println(mode);
+                
+                if(mode=='w'){
+                    //get the file name
+                    //write request
+                    int i = 0;
+                    for(i=1; i<512; i++){
+                        if (recvBuf[i] == 0){
+                            break;
+                        }
+                        else{
+                            fileName += (char) recvBuf[i];
+                        }
+                    }
+                    System.out.println(fileName);
+                    
+                    //send akt
+                    String aktstring = String.valueOf(0);
+                    int len = aktstring.length();                                             // length of the byte array
+                    byte[] buf = new byte[len];                                             // byte array that will store the data to be sent back to the client
+                    System.arraycopy(aktstring.getBytes(), 0, buf, 0, len);
 
-                socket.send(packet);
+                    InetAddress addr = packet.getAddress();
+                    int srcPort = packet.getPort();
 
-                counter++;
+                    packet.setData(buf);
+
+                    packet.setAddress(addr);
+                    packet.setPort(srcPort);
+
+                    socket.send(packet);
+                    
+                    write(fileName);
+                    
+                }
             }
         } catch (IOException e) {
             System.err.println(e);
         }
         socket.close();
+    }
+    
+    
+    public void write(String fileName) throws IOException{
+        FileOutputStream f = null;
+        try {   
+            f = new FileOutputStream("../"+fileName);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error creating file");
+            System.exit(1);
+        }
+        
+        
+        int counter=1;
+        outerloop:
+        while (true){
+            socket.setSoTimeout(2000);
+            boolean checktimeout = true;
+            
+            byte[] recvBuf = new byte[512]; 
+            DatagramPacket packet = new DatagramPacket(recvBuf, 512);
+            while (checktimeout){
+                try{
+                    socket.receive(packet);
+                    checktimeout = false;
+                } catch(SocketTimeoutException s){
+                    //end of transmission
+                    System.out.println("end");
+                    break outerloop;
+                }
+            }
+            f.write(recvBuf);
+            
+            //send akt
+            String aktstring = String.valueOf(counter);
+            int len = aktstring.length();                                             // length of the byte array
+            byte[] buf = new byte[len];                                             // byte array that will store the data to be sent back to the client
+            System.arraycopy(aktstring.getBytes(), 0, buf, 0, len);
+
+            InetAddress addr = packet.getAddress();
+            int srcPort = packet.getPort();
+
+            packet.setData(buf);
+
+            packet.setAddress(addr);
+            packet.setPort(srcPort);
+
+            socket.send(packet);
+            counter++;
+        }
+        
+    }
+    
+    
+    public void read(String fileName){
+        
     }
     
 }
