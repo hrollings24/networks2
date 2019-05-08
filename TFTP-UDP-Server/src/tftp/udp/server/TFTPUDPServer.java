6a126a43250f1e462f857b156b68ad4bf8c4a968
Harry Rollings <harryrollings@icloud.com>
2019-05-08 09:16:03 +0100

    final version

--- TFTP-UDP-Server/src/tftp/udp/server/TFTPUDPServer.java
+++ TFTP-UDP-Server/src/tftp/udp/server/TFTPUDPServer.java
@@ -59,6 +59,7 @@ public class TFTPUDPServer extends Thread {
             // run forever
             while (true) {
 
+                //packet recieved
                 DatagramPacket packet = new DatagramPacket(recvBuf, 516);
                 socket.receive(packet);
                 
@@ -90,9 +91,10 @@ public class TFTPUDPServer extends Thread {
 
                     packet.setAddress(addr);
                     packet.setPort(srcPort);
-
+                    
                     socket.send(packet);
                     
+                    //go to write function
                     write(fileName);
                     
                 }
@@ -110,22 +112,11 @@ public class TFTPUDPServer extends Thread {
                         }
                     }
                     
-                    //send akt
-                    String aktstring = String.valueOf(0);
-                    int len = aktstring.length();                                             // length of the byte array
-                    byte[] buf = new byte[len];                                             // byte array that will store the data to be sent back to the client
-                    System.arraycopy(aktstring.getBytes(), 0, buf, 0, len);
-
+                    
                     InetAddress addr = packet.getAddress();
                     int srcPort = packet.getPort();
 
-                    packet.setData(buf);
-
-                    packet.setAddress(addr);
-                    packet.setPort(srcPort);
-
-                    socket.send(packet);
-
+                    //go to read funtion
                     read(fileName, addr, srcPort);
                     
                     
@@ -143,6 +134,7 @@ public class TFTPUDPServer extends Thread {
     
     
     public void write(String fileName) throws IOException{
+        //create a new file with specified filename 
         FileOutputStream f = null;
         try {   
             f = new FileOutputStream(fileName);
@@ -151,23 +143,28 @@ public class TFTPUDPServer extends Thread {
             System.exit(1);
         }
         
-        
+        //counter is used to keep track of the block number
         int counter=0;
         outerloop:
         while (true){
+            //socket will timeout if nothing recieved within 2000ms
             socket.setSoTimeout(2000);
+            //a flag to check right packet recieved
             boolean checktimeout = true;
             
+            //the byte array and packet which will store the recieved data and packet
             byte[] recvBuf = new byte[516]; 
             DatagramPacket packet = new DatagramPacket(recvBuf, 516);
             while (checktimeout){
                 try{
+                    //recieve the packet
                     socket.receive(packet);
                     recvBuf = packet.getData();
-                    
+                    //if the packet is data type
                     if (((int)recvBuf[1]) == 3){
                         int val = ((recvBuf[2] & 0xff) << 8) | (recvBuf[3] & 0xff);
                         if (val == counter){
+                            //check whether correct block number and break from loop if correct
                             checktimeout = false;
                         }
                     }
@@ -177,6 +174,7 @@ public class TFTPUDPServer extends Thread {
                     break outerloop;
                 }
             }
+            //write the data to the file
             for(int i=4; i<516; i++){
                 f.write(recvBuf[i]);
             }
@@ -198,6 +196,7 @@ public class TFTPUDPServer extends Thread {
             packet.setAddress(addr);
             packet.setPort(srcPort);
 
+            //send the akt and increment the expected block number
             socket.send(packet);
             counter++;
         }
@@ -205,7 +204,9 @@ public class TFTPUDPServer extends Thread {
     }
     
     
-    public void read(String fileName, InetAddress addr, int src) throws IOException{
+    public void read(String fileName, InetAddress address, int port) throws IOException{
+        
+        //find the file at the specified filename
         FileInputStream file = null; 
         try{
             file = new FileInputStream(fileName);
@@ -216,42 +217,75 @@ public class TFTPUDPServer extends Thread {
         }
 
 
-        DatagramPacket packets[];
         int len = 512;
-
+        
         int avaliableBytes = file.available();
+        //the amount of packets expected to read
         int plength = (avaliableBytes/512) + 1;
-        packets = new DatagramPacket[plength];
+        //create array of packets to send to client
+        DatagramPacket[] packets = new DatagramPacket[plength];
+
+        //count is amount of bytes
+        //counter is block number tracket
+        int count = 0;
+        int counter = 0;
+        for(int i=0; i<avaliableBytes; i+=len){
+            byte[] buf = new byte[516];
+            buf[0] = (byte) 0;
+            buf[1] = (byte) 3;
+            //block code
+            buf[3] = (byte) (counter & 0xFF);
+            buf[2] = (byte) ((counter >> 8) & 0xFF);
+            
+            int x = file.read(buf, 4, len);
+            //create a packet and add to the array at the block number
+            packets[counter] = new DatagramPacket(buf, 516, address, port);
+            //increment variables
+            counter++;
+            count += x;
+            
+        }        
         
-        int counterq = 0;
-        for(int e=0; e<avaliableBytes; e+=len){
-            byte[] buf = new byte[len];
-            int x = file.read(buf, 0, len);
-            packets[counterq] = new DatagramPacket(buf, len, addr, src);
-            counterq++;
-        }
-        
-        Boolean check;
+        //send the packets
+        outerloop:
         for(int k=0; k<packets.length; k++){
+            //set socket timeout to 2000ms
             socket.setSoTimeout(2000);
-            check = true;
+            //check is a flag to check correct packet recieved
+            Boolean check = true;
             
             while (check){
-                System.out.println("Sending " + k);
+                //if packet is empty
+                if (packets[k] == null){
+                    //send empty
+                    byte[] b = new byte[0];
+                    packets[k] = new DatagramPacket(b, 0, address, port);
+                    //transmisson over
+                    socket.close();
+                    break;
+                    
+                }
+                //send packet
                 socket.send(packets[k]);
                 try{
+                    //recieve akt
                     socket.receive(packets[k]);
-                    check = false;
+                    byte[] b = new byte[4];
+                    b = packets[k].getData();
+                    //check packet is akt type and block number is correct
+                    int val = ((b[2] & 0xff) << 8) | (b[3] & 0xff);
+                    if(val == k){
+                        check = false;
+                    }
                 }
                 catch(SocketTimeoutException s){
                     System.out.println("error");
                 }
             }
-            
         }
         
         
-        
     }
+    
     
 }
